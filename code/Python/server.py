1a2dd753a4072f79ab58958dc547b3bf92b7c853
Daniil Nikiforov <dgallardonikiforov@gmail.com>
2024-04-20 13:22:00 +0200

    Modificado el servidor para que aloje en cualquier dirección del localhost en un puerto determinado

--- code/Python/server.py
+++ code/Python/server.py
@@ -5,6 +5,9 @@ from queue import Queue
 from datetime import datetime
 from tabulate import tabulate
 
+# Puerto para la recepción de datos
+puerto = 8888
+ip_addr = "0.0.0.0"
 # Diccionario para almacenar las colas por tipo de sensor
 
 sensor_queues = {
@@ -82,9 +85,9 @@ def handle_client(client_socket):
 
 def server():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind(("127.0.0.1", 8080))
+    server_socket.bind((ip_addr, puerto))
     server_socket.listen(5)
-    print("El servidor está escuchando en el puerto 8080")
+    print(f"El servidor está escuchando en el puerto {puerto}")
 
     while True:
         client_socket, addr = server_socket.accept()
@@ -94,41 +97,3 @@ def server():
 # Iniciar el servidor y la función de impresión en hilos separados
 server_thread = threading.Thread(target=server)
 server_thread.start()
-
-
-'''
-Zona de prueba de códigos
-    # Diccionario para almacenar los datos de los sensores por habitación
-    sensor_dict = {}
-    
-    # Recopilar datos de los sensores para cada habitación
-
-    for sensor in lista_sensores:
-        try:
-            sensor_type, sensor_value = sensor.split(':')
-            if room not in sensor_dict:
-                sensor_dict[room] = {}
-            if sensor_type not in sensor_dict[room]:
-                sensor_dict[room][sensor_type] = []
-            sensor_dict[room][sensor_type].append(sensor_value)
-        except ValueError:
-            # Si el split falla, imprimir el mensaje del request directamente
-            print(request)
-            break  # Salir del bucle para evitar procesar más datos
-
-
-    # Construir la tabla con los datos de los sensores
-    for room, sensors in sensor_dict.items():
-        table_headers = ["Sensor", "Valor"]
-        table_rows = []
-        for sensor_type, sensor_values in sensors.items():
-            table_headers.append(sensor_type)
-            table_rows.append([sensor_type] + sensor_values)
-
-        with open(f"{room}.txt", "a") as file:
-            file.write(f"\n{hour}\n")
-            file.write(tabulate(table_rows, headers=table_headers, tablefmt="grid"))
-        #print(f"Habitación: {room} a las {hour}")
-        #print(tabulate(table_rows, headers=table_headers, tablefmt="grid"))
-        #print()  # Añadir línea en blanco entre tablas de habitaciones
-'''
